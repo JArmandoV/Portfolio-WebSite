0e0ef61ee75f4600f599157842bea78671cd31ed
Armando <A01253219@tec.mx>
2023-04-11 14:06:45 -0700

    Changed to pages dising

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,26 @@
+import {
+  About, Contact, Experience,
+  Hero, Navbar, Tech, Works, StarsCanvas
+} from '../../components'
+
+const Home = () => {
+  return (
+    <div className="relative z-0 bg-primary">
+      
+        <div className="bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+  )
+}
+
+export default Home
